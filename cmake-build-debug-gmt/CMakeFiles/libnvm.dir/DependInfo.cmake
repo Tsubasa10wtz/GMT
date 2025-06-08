656ff9ae6fcb2098f8034f22783c804c7dd623cd
wangtianze197@gmail.com <2868443064@qq.com>
2025-06-08 17:00:46 +0800

    Initial commit(GMT)

---
+++ cmake-build-debug-gmt/CMakeFiles/libnvm.dir/DependInfo.cmake
@@ -0,0 +1,41 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/tzwang/GMT-main/src/admin.cpp" "/home/tzwang/GMT-main/cmake-build-debug-gmt/CMakeFiles/libnvm.dir/src/admin.cpp.o"
+  "/home/tzwang/GMT-main/src/ctrl.cpp" "/home/tzwang/GMT-main/cmake-build-debug-gmt/CMakeFiles/libnvm.dir/src/ctrl.cpp.o"
+  "/home/tzwang/GMT-main/src/dma.cpp" "/home/tzwang/GMT-main/cmake-build-debug-gmt/CMakeFiles/libnvm.dir/src/dma.cpp.o"
+  "/home/tzwang/GMT-main/src/error.cpp" "/home/tzwang/GMT-main/cmake-build-debug-gmt/CMakeFiles/libnvm.dir/src/error.cpp.o"
+  "/home/tzwang/GMT-main/src/linux/device.cpp" "/home/tzwang/GMT-main/cmake-build-debug-gmt/CMakeFiles/libnvm.dir/src/linux/device.cpp.o"
+  "/home/tzwang/GMT-main/src/linux/dma.cpp" "/home/tzwang/GMT-main/cmake-build-debug-gmt/CMakeFiles/libnvm.dir/src/linux/dma.cpp.o"
+  "/home/tzwang/GMT-main/src/mutex.cpp" "/home/tzwang/GMT-main/cmake-build-debug-gmt/CMakeFiles/libnvm.dir/src/mutex.cpp.o"
+  "/home/tzwang/GMT-main/src/queue.cpp" "/home/tzwang/GMT-main/cmake-build-debug-gmt/CMakeFiles/libnvm.dir/src/queue.cpp.o"
+  "/home/tzwang/GMT-main/src/rpc.cpp" "/home/tzwang/GMT-main/cmake-build-debug-gmt/CMakeFiles/libnvm.dir/src/rpc.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "_CUDA"
+  "__CUDA__"
+  "libnvm_EXPORTS"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "include"
+  "../include"
+  "../include/freestanding/include"
+  "/usr/local/cuda/include"
+  "/usr/local/cuda-12.4/include"
+  "../src"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
